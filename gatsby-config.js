d1ddb12149e7df7260ed1bcb865d5e9af6066809
Faris Mohamed <farismohamed@riafy.me>
2020-04-17 22:32:31 +0530

    connected gatsby with netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
   plugins: [`gatsby-plugin-netlify-cms`],
 }
+plugins: [
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `markdown-pages`,
+      path: `${__dirname}/blog/src/markdown-pages`,
+    },
+  },
+  `gatsby-transformer-remark`,
+]
